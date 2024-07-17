7895c8866534267c55612d4d33e50bf9e23d418a
Akihiro Urushihara <a.urusihara@gmail.com>
2024-07-17 17:22:26 +0000

    by GitHub Action CI (SHA 412f02d5b20fa97aa77aeb9c6e78f41ad90eecb7)

---
+++ kmisskey-stream.js
@@ -0,0 +1,15 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd)
+    define(['exports'], factory);
+  else if (typeof exports === 'object')
+    factory(module.exports);
+  else
+    root['kmisskey-stream'] = factory(typeof this['kmisskey-stream'] === 'undefined' ? {} : this['kmisskey-stream']);
+}(globalThis, function (_) {
+  'use strict';
+  //region block: pre-declaration
+  //endregion
+  return _;
+}));
+
+//# sourceMappingURL=kmisskey-stream.js.map
